7ee4941200ee5aacc94d9d0debfde5fcee5f034b
Tazztikl <tatu.tikka2@metropolia.fi>
2023-10-18 21:10:04 +0300

    ravintolat juttui

--- api.js
+++ api.js
@@ -1,8 +1,8 @@
-import {errorModal, restaurantModal, restaurantRow, showTodaysMenu} from './components.js';
-import {fetchData} from './functions.js';
-import {apiUrl, positionOptions} from './variables.js';
+import { errorModal, restaurantModal, restaurantRow, showTodaysMenu } from './components.js';
+import { fetchData } from './functions.js';
+import { apiUrl, positionOptions } from './variables.js';
 
-const modal = document.querySelector('dialog');
+const modal2 = document.querySelector('dialog');
 
 const calculateDistance = (x1, y1, x2, y2) =>
   Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
@@ -35,12 +35,12 @@ const createTable = restaurants => {
           apiUrl + `/restaurants/daily/${restaurant._id}/fi`);
         console.log(menuday);
 
-          
+
         const menuHtml = restaurantModal(restaurant, menu);
         modal.insertAdjacentHTML('beforeend', menuHtml);
 
         const button2 =
-        document.getElementById("button2");
+          document.getElementById("button2");
         button2.addEventListener("click", ShowDay);
 
         const sulje = document.getElementById('sulje')
@@ -50,8 +50,8 @@ const createTable = restaurants => {
 
         function ShowDay() {
           modal.removeChild(document.getElementById("viikko"))
-          const menuHtml = showTodaysMenu (restaurant, menuday);
-          modal.insertAdjacentHTML('beforeend', menuHtml); 
+          const menuHtml = showTodaysMenu(restaurant, menuday);
+          modal.insertAdjacentHTML('beforeend', menuHtml);
         }
 
         modal.showModal();
@@ -75,56 +75,56 @@ const error = err => {
 
 
 
-const success = async pos => {
-  try {
-    const crd = pos.coords;
-    const restaurants = await fetchData(apiUrl + '/restaurants');
-    console.log(restaurants);
-    restaurants.sort((a, b) => {
-      const x1 = crd.latitude;
-      const y1 = crd.longitude;
-      const x2a = a.location.coordinates[1];
-      const y2a = a.location.coordinates[0];
-      const distanceA = calculateDistance(x1, y1, x2a, y2a);
-      const x2b = b.location.coordinates[1];
-      const y2b = b.location.coordinates[0];
-      const distanceB = calculateDistance(x1, y1, x2b, y2b);
-      return distanceA - distanceB;
-    });
-    createTable(restaurants);
-    // buttons for filtering
-    const sodexoBtn = document.querySelector('#sodexo');
-    const compassBtn = document.querySelector('#compass');
-    const resetBtn = document.querySelector('#reset');
+// const success = async pos => {
+//   try {
+//     const crd = pos.coords;
+const restaurants = await fetchData(apiUrl + '/restaurants');
+//     console.log(restaurants);
+//     restaurants.sort((a, b) => {
+//       const x1 = crd.latitude;
+//       const y1 = crd.longitude;
+//       const x2a = a.location.coordinates[1];
+//       const y2a = a.location.coordinates[0];
+//       const distanceA = calculateDistance(x1, y1, x2a, y2a);
+//       const x2b = b.location.coordinates[1];
+//       const y2b = b.location.coordinates[0];
+//       const distanceB = calculateDistance(x1, y1, x2b, y2b);
+//       return distanceA - distanceB;
+//     });
+createTable(restaurants);
+// buttons for filtering
+const sodexoBtn = document.querySelector('#sodexo');
+const compassBtn = document.querySelector('#compass');
+const resetBtn = document.querySelector('#reset');
 
-    sodexoBtn.addEventListener('click', () => {
-      const sodexoRestaurants = restaurants.filter(
-        restaurant => restaurant.company === 'Sodexo'
-      );
-      console.log(sodexoRestaurants);
-      createTable(sodexoRestaurants);
-    });
+sodexoBtn.addEventListener('click', () => {
+  const sodexoRestaurants = restaurants.filter(
+    restaurant => restaurant.company === 'Sodexo'
+  );
+  console.log(sodexoRestaurants);
+  createTable(sodexoRestaurants);
+});
 
-    compassBtn.addEventListener('click', () => {
-      const compassRestaurants = restaurants.filter(
-        restaurant => restaurant.company === 'Compass Group'
-      );
-      console.log(compassRestaurants);
-      createTable(compassRestaurants);
-    });
+compassBtn.addEventListener('click', () => {
+  const compassRestaurants = restaurants.filter(
+    restaurant => restaurant.company === 'Compass Group'
+  );
+  console.log(compassRestaurants);
+  createTable(compassRestaurants);
+});
 
-    resetBtn.addEventListener('click', () => {
-      createTable(restaurants);
-    });
+resetBtn.addEventListener('click', () => {
+  createTable(restaurants);
+});
 
-  } catch (error) {
-    modal.innerHTML = errorModal(error.message);
-    modal.showModal();
-  }
-};
+// } catch (error) {
+//   modal.innerHTML = errorModal(error.message);
+modal.showModal2();
+  // }
+// };
 
 
-navigator.geolocation.getCurrentPosition(success, error, positionOptions);
+// navigator.geolocation.getCurrentPosition(success, error, positionOptions);
 
 
 
